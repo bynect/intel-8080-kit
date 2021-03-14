ff3eb9cc411fbfa856cfa922429c0bcacb14025d
bynect <68197565+bynect@users.noreply.github.com>
2021-03-14 03:50:21 +0100

    Add initial cli

---
+++ src/bin/dis8080.rs
@@ -0,0 +1,34 @@
+use asm_8080::dis::{disassemble, disassemble_raw};
+use std::{env, fs, path::Path};
+
+fn main() {
+    let mut args = env::args().collect::<Vec<_>>();
+    let raw = if args.len() > 1 && args[1] == "--raw" {
+        args.remove(1);
+        true
+    } else {
+        false
+    };
+
+    for arg in &args[1..] {
+        let path = Path::new(&arg);
+
+        if path.exists() {
+            let bin = fs::read(arg).unwrap();
+
+            if raw {
+                let out = disassemble_raw(&bin);
+                out.iter().for_each(|&x| print!("{:02x} ", x));
+                println!("\n{:?}", bin);
+            } else {
+                if let Ok(out) = disassemble(&bin) {
+                    println!("{:?}", out);
+                } else {
+                    eprintln!("Erroneous binary file.");
+                }
+            }
+        } else {
+            eprintln!("{} doesn't exist.", arg);
+        }
+    }
+}
